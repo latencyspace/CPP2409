cf49c3f9ae6598207883f79b47550f1c311b884c
Latency <latency@kakao.com>
2024-10-02 16:49:58 +0900

    2-d Array and Tic Tac Toe

---
+++ project/week5/tic_tac_toe3.cpp
@@ -0,0 +1,132 @@
+// Tic Tac Toc 3인 게임 (가산점)
+// tic_tac_toe에 게임 인원을 3명으로 늘리기(O, X, V)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int numCell = 3;
+    char board[numCell][numCell]{};
+    int x, y; // 사용자에게 입력받은 X, Y 좌표를 저장할 변수
+
+    // 보드판 초기화
+    for (x = 0; x < numCell; x++)
+    {
+        for (y = 0; y < numCell; y++)
+        {
+            board[x][y] = ' ';
+        }
+    }
+
+    // 게임 코드
+    int k = 0;                              // 누구 차례인지 체크하기 위한 변수
+    char currentUser;                       // 현재 유저의 돌을 저장하기 위한 문자 변수
+    const char players[] = {'X', 'O', 'V'}; // 플레이어 배열
+
+    while (true)
+    {
+        // 1. 누구 차례인지 출력
+        currentUser = players[k % 3];
+        cout << (currentUser == 'X' ? "첫 번째 유저(X)" : (currentUser == 'O' ? "두 번째 유저(O)" : "세 번째 유저(V)")) << "의 차례입니다 -> ";
+
+        // 2. 좌표 입력 받기
+        cout << "(X, Y) 좌표를 입력하세요: ";
+        cin >> x >> y;
+
+        // 3. 입력받은 좌표의 유효성 체크
+        if (x >= numCell || y >= numCell)
+        {
+            cout << x << ", " << y << ": ";
+            cout << "X와 Y 둘 중 하나가 칸을 벗어납니다." << endl;
+            continue;
+        }
+        if (board[x][y] != ' ')
+        {
+            cout << x << ", " << y << ": 이미 돌이 차있습니다." << endl;
+            continue;
+        }
+
+        // 4. 입력받은 좌표에 현재 유저의 돌 놓기
+        board[x][y] = currentUser;
+
+        // 5. 현재 보드판 출력
+        for (int i = 0; i < numCell; i++)
+        {
+            cout << "---|---|---" << endl;
+            for (int j = 0; j < numCell; j++)
+            {
+                cout << board[i][j];
+                if (j == numCell - 1)
+                {
+                    break;
+                }
+                cout << "  |";
+            }
+            cout << endl;
+        }
+        cout << "---|---|---" << endl;
+
+        // 6. 승자 확인 함수 호출
+        bool Winner = false;
+
+        // 6-1. 가로 체크
+        for (int i = 0; i < numCell; i++)
+        {
+            if (board[i][0] == currentUser && board[i][1] == currentUser && board[i][2] == currentUser)
+            {
+                Winner = true;
+                break;
+            }
+        }
+        // 6-2. 세로 체크
+        for (int i = 0; i < numCell; i++)
+        {
+            if (board[0][i] == currentUser && board[1][i] == currentUser && board[2][i] == currentUser)
+            {
+                Winner = true;
+                break;
+            }
+        }
+        // 6-3. 대각선 체크
+        if (board[0][0] == currentUser && board[1][1] == currentUser && board[2][2] == currentUser)
+        {
+            Winner = true;
+        }
+        if (board[0][2] == currentUser && board[1][1] == currentUser && board[2][0] == currentUser)
+        {
+            Winner = true;
+        }
+
+        // 6-4. 승자가 결정된 경우
+        if (Winner)
+        {
+            cout << "승자: " << currentUser << "!" << endl;
+            break;
+        }
+
+        // 7. 모든 칸이 찼는지 확인
+        bool isFull = true;
+        for (int i = 0; i < numCell; i++)
+        {
+            for (int j = 0; j < numCell; j++)
+            {
+                if (board[i][j] == ' ')
+                {
+                    isFull = false;
+                    break;
+                }
+            }
+        }
+
+        // 7-1. 모든 칸이 찼다면
+        if (isFull)
+        {
+            cout << "모든 칸이 찼습니다. 게임 종료!" << endl;
+            break;
+        }
+
+        k++;
+    }
+    return 0;
+}
